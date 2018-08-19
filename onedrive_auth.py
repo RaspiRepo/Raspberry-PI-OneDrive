7dd36a5d9f8df7f6e154f1c576f19de0c4007cce
Mariya Krishnasamy <mariya.k@skuchain.com>
2018-08-18 20:09:03 -0700

    revised one drive authentication

--- onedrive_auth.py
+++ onedrive_auth.py
@@ -1,15 +1,17 @@
 __author__ = 'RaspiRepo'
 import onedrivesdk
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 #Onedrive Access
 redirect_uri = "https://login.live.com/oauth20_desktop.srf"
 client_secret = "client_secret"
-client_id_str = "client_id_str"
+client_id_str = "client_id"
 
 
-user_name = 'user_name@example.com'
-password = 'password'
+user_name = 'username'
+password = 'your password'
 
 client = onedrivesdk.get_default_client(client_id=client_id_str,
                                         scopes=['wl.signin',
@@ -18,25 +20,37 @@ client = onedrivesdk.get_default_client(client_id=client_id_str,
 auth_url = client.auth_provider.get_auth_url(redirect_uri)
 driver = webdriver.PhantomJS()  # 'phantomjs')
 
+driver.set_window_size(320,240)
+driver.maximize_window()
 
-#driver = webdriver.Chrome('chromedriver')
-#driver.set_window_size(1, 1)
-
+# chrome_driver = webdriver.Chrome('chromedriver')
+# chrome_driver.set_window_size(2, 2)
 
 driver.get(auth_url)
-driver.implicitly_wait(15)
+driver.implicitly_wait(8)
+driver.get(auth_url)
 
 login_field = driver.find_element_by_name("loginfmt")
-password_field = driver.find_element_by_name("passwd")
-sign_btn = driver.find_element_by_id("idSIButton9")
-
-
-# Fill user name and password
 login_field.send_keys(user_name)
-password_field.send_keys(password)
-sign_btn.click()
-element = driver.find_element_by_name('ucaccept')
-element.click()
+
+next_btn = driver.find_element(By.ID, "idSIButton9")
+next_btn.click()
+time.sleep(2)
+
+psw_field = driver.find_element_by_name("passwd")
+psw_field.send_keys(password)
+
+
+signin_btn = driver.find_element(By.ID, "idSIButton9")
+signin_btn.click()
+time.sleep(3)
+driver.save_screenshot('authrization.png')
+
+idBtn_Accept = driver.find_element(By.ID, "idBtn_Accept")
+idBtn_Accept.click()
+time.sleep(3)
+
+print(driver.current_url)
 
 # parse URL to get code and close the browser
 tokens = driver.current_url.split('=')
@@ -46,4 +60,10 @@ driver.quit()
 access_code = tokens[1].split('&')[0]
 
 print("access code is here ", access_code)
+
+# authenticate the client
+onedrive_conn = client.auth_provider.authenticate(access_code, redirect_uri, client_secret)
+returned_item = client.item(drive='me', id='root').children['authrization.png'].upload('./authrization.png')
+
+
 
